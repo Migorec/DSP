67ea2f06454094450eaff3e8cbeeaffa738b6d01
MigOrec <MigOrec@gmail.com>
2012-10-28 17:48:53 +0400

    поправил все фильтры

--- Lab04.hs
+++ Lab04.hs
@@ -6,7 +6,7 @@ import Graphics.Gnuplot.Simple
 import Graphics.Gnuplot.Terminal.WXT
 import Noise
 import Filter.Convolution
-import Filter.IIR
+import qualified Filter.IIR as IIR
 import Filter.FIR
 import Fourier
 
@@ -21,16 +21,19 @@ main = do
     let i = ind nu
         --i = zipWith (*) di $ map (\t -> exp (0 :+ pi*(t/0.1))) [-3, -3 + 0.1 ..  3]
         n = length i
-        --rnu =  highBatterworth nu 0.1
-        rnu = nu{ind = rfft $ zipWith (*) (fft  i) ( gaus 0.5 (1/(dt nu)/fromIntegral n))}
+        
+--        rnu =  lowGaus nu 0.5
+        --rnu =  highGaus nu 20
+        rnu = IIR.highBatterworth nu 0.5
+        --rnu = nu{ind = rfft $ zipWith (*) (fft  i) ( gaus 0.5 (1/(dt nu)/fromIntegral n))}
 --    print rnu
     let
         dftu = zip [-3,-3+0.05 .. 3] $ map magnitude $ ind nu
         rdftu = zip [-3,-3+0.05 .. 3] $ map magnitude $ ind rnu
     --    rdftu = map (\x -> (x,evalA rnu x)) [-3,-3+0.01 .. 3]  
         
-        st1 = defaultStyle {lineSpec = CustomStyle [LineTitle "Convolution", LineWidth 3.0]}
-        st2 = defaultStyle {lineSpec = CustomStyle [LineTitle "Convolution", LineWidth 2.0]}
+        st1 = defaultStyle {lineSpec = CustomStyle [LineTitle "Noise", LineWidth 3.0]}
+        st2 = defaultStyle {lineSpec = CustomStyle [LineTitle "Filter", LineWidth 2.0]}
     
     
     plotPathsStyle [terminal cons] [(st1,dftu),(st2,rdftu)] 
