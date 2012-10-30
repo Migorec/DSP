772cdc14f2cc741a67e64cc89191d92d165f883a
MigOrec <MigOrec@gmail.com>
2012-10-30 16:36:17 +0400

    теперь шум генерируется и накладвается тдельно

--- Noise.hs
+++ Noise.hs
@@ -6,6 +6,25 @@ import Control.Monad
 import System.Random
 import Data.Random.Normal
 
+
+uniformNoise :: Int -> Double -> IO [Complex Double]
+uniformNoise n a =
+    do replicateM n $ do r <- randomIO :: IO Double
+                         if r > (5 / fromIntegral n)
+                         then return 0
+                         else do r <- randomIO :: IO Double
+                                 return (r*a :+ 0)
+                                 
+normalNoise :: Int -> Double -> Double -> IO [Complex Double]
+normalNoise n a s = 
+    do replicateM n $ do r <- normalIO' (a,s)
+                         return (r :+ 0)
+
+applyNoise :: DSignal -> [Complex Double] -> DSignal
+applyNoise (DSignal t0 dt l) noise = DSignal t0 dt $ zipWith (+) l noise 
+
+
+{-
 uniformNoise :: Double -> DSignal -> IO DSignal
 uniformNoise a s = 
     do print n
@@ -25,3 +44,4 @@ normalNoise a s sig =
                               return $ v + (r :+ 0)
                     ) $ ind sig
        return sig{ind = nind}
+-}       
