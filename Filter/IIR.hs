ff6a8de91cb7f8b858dbafbac28e98ee641c71c1
MigOrec <MigOrec@gmail.com>
2012-10-30 14:54:45 +0400

    отрефакторил бих-фильтры

--- Filter/IIR.hs
+++ Filter/IIR.hs
@@ -1,4 +1,4 @@
-module Filter.IIR where
+module Filter.IIR (highButterworth, lowButterworth) where
 
 
 import Data.Complex
@@ -6,60 +6,32 @@ import Signals
 import Fourier
 import Filter.Convolution
 
-lowBatterworth :: DSignal -> Double -> DSignal
-lowBatterworth (DSignal t0 tau l) b = 
-        DSignal t0 tau $ rfft $ zipWith (*) (fft l) $ 
-                                            map (\f -> b0 / (1 + 
-                                                             a1*exp(-(0 :+ 2*pi*f*tau)) + 
-                                                             a2*exp(-(0 :+ 4*pi*f*tau))
-                                                            )
-                                                ) [0, df .. (n-1)*df]
+sFilter :: DSignal -> (Double -> Complex Double) -> DSignal
+sFilter (DSignal t0 tau l) h = 
+    DSignal t0 tau $ rfft $ zipWith (*) (fft l) $ map h [0,df .. (n-1)*df]
+        where n = fromIntegral $ length l
+              df = 1 / n / tau
+
+iirResponse b0 a1 a2 tau f =  b0 / (1 + a1*exp(-(0 :+ 2*pi*f*tau)) + a2*exp(-(0 :+ 4*pi*f*tau)))
+
+butterworthCoef tau b = (b0, a1,a2) 
     where fi = pi / 4
           fact = sin(pi*b*tau)
           q = fact*sin(fi)
           c = 1 - fact*fact
           d = (-c + sqrt(c*c + 4*q*q))/2
-          e =  sqrt(d) + sqrt(d+1) --sqrt(d*(d+1))--	
-          a1 = -((2*q*q/d - 1)*2/e/e :+ 0)
+          e =  sqrt(d) + sqrt(d+1) 
+          a1 = (2*q*q/d - 1)*2/e/e :+ 0
           a2 = 1/e/e/e/e :+ 0
-          b0 = (1 + a1 + a2) 
-          n = fromIntegral $ length l
-          df = 1 / n / tau
+          b0 = 1 + a1 + a2
+
+lowButterworth :: DSignal -> Double -> DSignal
+lowButterworth sig@(DSignal t0 tau l) b = sFilter sig (iirResponse (b0-2*a1) (-a1) a2 tau)
+    where 
+          (b0,a1,a2) = butterworthCoef tau b
           
-          
-highBatterworth :: DSignal -> Double -> DSignal
-highBatterworth (DSignal t0 tau l) b = 
-        DSignal t0 tau $  rfft $ zipWith (*) (fft l) $ 
-                                            map (\f -> b0 / (1 + 
-                                                             a1*exp(-(0 :+ 2*pi*f*tau)) + 
-                                                             a2*exp(-(0 :+ 4*pi*f*tau))
-                                                            )
-                                                ) [0, df .. (n-1)*df]
-    where fi = pi / 4
-          fact = sin(pi*b*tau)
-          q = fact*sin(fi)
-          c = 1 - fact*fact
-          d = (-c + sqrt(c*c + 4*q*q))/2
-          e = sqrt(d) + sqrt(d+1) --sqrt(d*(d+1))-- 	
-          a1 = ((2*q*q/d - 1)*2/e/e :+ 0)
-          a2 = 1/e/e/e/e :+ 0
-          b0 = (1 + a1 + a2) 
-          n = fromIntegral $ length l
-          df = 1 / n / tau          
-{-
-batterworth :: Double -> Double -> [Complex Double]
-batterworth b tau = map (\f -> b0 / (1 + 
-                                     a1*exp(-(0 :+ 2*pi*f*tau)) + 
-                                     a2*exp(-(0 :+ 4*pi*f*tau))
-                                    )
-                        ) [0,tau ..]
-    where fi = pi / 4
-          fact = sin(pi*b*tau)
-          q = fact*sin(fi)
-          c = 1 - fact*fact
-          d = (-c + sqrt(c*c + 4*q*q))/2
-          e =  sqrt(d) + sqrt(d+1) --sqrt(d*(d+1))--	
-          a1 = -((2*q*q/d - 1)*2/e/e :+ 0)
-          a2 = 1/e/e/e/e :+ 0
-          b0 = (1 + a1 + a2) 
-          -}
+highButterworth :: DSignal -> Double -> DSignal
+highButterworth sig@(DSignal t0 tau l) b = sFilter sig (iirResponse b0 a1 a2 tau)
+    where 
+          (b0,a1,a2) = butterworthCoef tau b          
+
